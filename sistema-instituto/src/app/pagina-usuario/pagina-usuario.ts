3cfb3cbc4c36ad3d78ee6572f6e4afa0613d7cd0
Nxjose18 <59268915+Nxjose18@users.noreply.github.com>
2026-07-08 03:06:42 +0000

    redistribucion de archivos v2.5

---
+++ sistema-instituto/src/app/pagina-usuario/pagina-usuario.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+import { AuthService } from '../services/auth';
+import { Router } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
+import { CommonModule } from '@angular/common';
+
+@Component({
+   standalone: true,
+  selector: 'app-pagina-usuario',
+  imports: [MatIconModule,CommonModule],
+  templateUrl: './pagina-usuario.html',
+  styleUrl: './pagina-usuario.css',
+})
+export class PaginaUsuario {
+  constructor(
+  private auth: AuthService,
+  private router: Router
+) {}
+
+cerrarSesion(){
+
+    this.auth.logout();
+
+    this.router.navigate(['/']);
+
+}
+
+ opcionSeleccionada: string = 'cursos'; 
+
+  // Función para cambiar la pestaña activa
+  seleccionar(opcion: string) {
+    this.opcionSeleccionada = opcion;
+  }
+}
